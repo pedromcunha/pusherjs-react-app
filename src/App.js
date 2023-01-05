5f67a07681db91ce73c6786316d167a70adc7988
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-01-05 11:49:32 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Pusher from "pusher-js";
+import { useEffect, useState } from "react";
+const pusher = new Pusher(process.env.REACT_APP_PUSHER_KEY, {
+  cluster: process.env.REACT_APP_PUSHER_CLUSTER,
+  wsHost: process.env.REACT_APP_WS_HOST,
+  forceTLS: true,
+  disableStats: true,
+  enabledTransports: ["ws", "wss"],
+});
+
+Pusher.logToConsole = true;
+
+const channel = pusher.subscribe(process.env.REACT_APP_PUSHER_CHANNEL);
+channel.bind_global(function (event, data) {
+  console.log("NEW EVENT: ", event);
+  console.log(data);
+});
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    channel.bind_global(function (event, data) {
+      console.log("NEW EVENT: ", event);
+      console.log(data);
+      setMessages((messages) => {
+        setMessages([...messages, { event, data }]);
+      });
+    });
+
+    return () => {
+      channel.unbind_global();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {messages.forEach((message, i) => {
+        <div key={i}>{JSON.stringify(message)}</div>;
+      })}
     </div>
   );
 }
